60fd05acfc98c9af64f37e2251db06297cb04b65
julio <l18330415@hermosillo.tecnm.mx>
2022-05-31 10:29:01 -0700

    Cambios a interfaz de ganaderia

--- PROYECTOQAG5/PGanado.Designer.cs
+++ PROYECTOQAG5/PGanado.Designer.cs
@@ -31,22 +31,9 @@
             System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
             System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
             this.btnagregarproducto = new FontAwesome.Sharp.IconButton();
-            this.Dgv_usuarios = new System.Windows.Forms.DataGridView();
-            this.btnseleccionar = new System.Windows.Forms.DataGridViewButtonColumn();
-            this.Id = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Usuario = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.NombreCompleto = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Correo = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Contraseña = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.IdRol = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Rol = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.EstadoValor = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Estado = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Ver = new System.Windows.Forms.DataGridViewButtonColumn();
-            this.Edit = new System.Windows.Forms.DataGridViewButtonColumn();
-            this.Eliminar = new System.Windows.Forms.DataGridViewButtonColumn();
             this.Btn_DetalleBovino = new FontAwesome.Sharp.IconButton();
-            ((System.ComponentModel.ISupportInitialize)(this.Dgv_usuarios)).BeginInit();
+            this.Dgv_ganado = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.Dgv_ganado)).BeginInit();
             this.SuspendLayout();
             // 
             // btnagregarproducto
@@ -61,159 +48,16 @@
             this.btnagregarproducto.IconColor = System.Drawing.Color.White;
             this.btnagregarproducto.IconFont = FontAwesome.Sharp.IconFont.Auto;
             this.btnagregarproducto.IconSize = 35;
-            this.btnagregarproducto.Location = new System.Drawing.Point(11, 11);
-            this.btnagregarproducto.Margin = new System.Windows.Forms.Padding(2);
+            this.btnagregarproducto.Location = new System.Drawing.Point(15, 14);
+            this.btnagregarproducto.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnagregarproducto.Name = "btnagregarproducto";
-            this.btnagregarproducto.Size = new System.Drawing.Size(177, 35);
+            this.btnagregarproducto.Size = new System.Drawing.Size(236, 43);
             this.btnagregarproducto.TabIndex = 104;
             this.btnagregarproducto.Text = "Agregar Bovino";
             this.btnagregarproducto.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             this.btnagregarproducto.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
             this.btnagregarproducto.UseVisualStyleBackColor = false;
             this.btnagregarproducto.Click += new System.EventHandler(this.btnagregarproducto_Click);
-            // 
-            // Dgv_usuarios
-            // 
-            this.Dgv_usuarios.AllowUserToAddRows = false;
-            this.Dgv_usuarios.BackgroundColor = System.Drawing.Color.White;
-            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
-            dataGridViewCellStyle1.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
-            dataGridViewCellStyle1.Padding = new System.Windows.Forms.Padding(2);
-            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
-            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
-            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
-            this.Dgv_usuarios.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
-            this.Dgv_usuarios.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.Dgv_usuarios.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-            this.btnseleccionar,
-            this.Id,
-            this.Usuario,
-            this.NombreCompleto,
-            this.Correo,
-            this.Contraseña,
-            this.IdRol,
-            this.Rol,
-            this.EstadoValor,
-            this.Estado,
-            this.Ver,
-            this.Edit,
-            this.Eliminar});
-            this.Dgv_usuarios.Location = new System.Drawing.Point(11, 77);
-            this.Dgv_usuarios.Margin = new System.Windows.Forms.Padding(2);
-            this.Dgv_usuarios.MultiSelect = false;
-            this.Dgv_usuarios.Name = "Dgv_usuarios";
-            this.Dgv_usuarios.ReadOnly = true;
-            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Control;
-            dataGridViewCellStyle2.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.WindowText;
-            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.Color.White;
-            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.Color.Black;
-            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
-            this.Dgv_usuarios.RowHeadersDefaultCellStyle = dataGridViewCellStyle2;
-            this.Dgv_usuarios.RowHeadersWidth = 16;
-            this.Dgv_usuarios.RowTemplate.Height = 28;
-            this.Dgv_usuarios.Size = new System.Drawing.Size(991, 661);
-            this.Dgv_usuarios.TabIndex = 105;
-            this.Dgv_usuarios.TabStop = false;
-            // 
-            // btnseleccionar
-            // 
-            this.btnseleccionar.FillWeight = 120F;
-            this.btnseleccionar.HeaderText = "";
-            this.btnseleccionar.Name = "btnseleccionar";
-            this.btnseleccionar.ReadOnly = true;
-            this.btnseleccionar.Width = 40;
-            // 
-            // Id
-            // 
-            this.Id.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
-            this.Id.HeaderText = "IdUsuario";
-            this.Id.Name = "Id";
-            this.Id.ReadOnly = true;
-            this.Id.Visible = false;
-            // 
-            // Usuario
-            // 
-            this.Usuario.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
-            this.Usuario.HeaderText = "Arete";
-            this.Usuario.Name = "Usuario";
-            this.Usuario.ReadOnly = true;
-            // 
-            // NombreCompleto
-            // 
-            this.NombreCompleto.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
-            this.NombreCompleto.HeaderText = "Nombre Completo";
-            this.NombreCompleto.Name = "NombreCompleto";
-            this.NombreCompleto.ReadOnly = true;
-            // 
-            // Correo
-            // 
-            this.Correo.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
-            this.Correo.HeaderText = "Sexo";
-            this.Correo.Name = "Correo";
-            this.Correo.ReadOnly = true;
-            // 
-            // Contraseña
-            // 
-            this.Contraseña.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
-            this.Contraseña.HeaderText = "Contraseña";
-            this.Contraseña.Name = "Contraseña";
-            this.Contraseña.ReadOnly = true;
-            this.Contraseña.Visible = false;
-            // 
-            // IdRol
-            // 
-            this.IdRol.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
-            this.IdRol.HeaderText = "IdRol";
-            this.IdRol.Name = "IdRol";
-            this.IdRol.ReadOnly = true;
-            this.IdRol.Visible = false;
-            // 
-            // Rol
-            // 
-            this.Rol.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
-            this.Rol.HeaderText = "ejemplo1";
-            this.Rol.Name = "Rol";
-            this.Rol.ReadOnly = true;
-            // 
-            // EstadoValor
-            // 
-            this.EstadoValor.HeaderText = "EstadoValor";
-            this.EstadoValor.Name = "EstadoValor";
-            this.EstadoValor.ReadOnly = true;
-            this.EstadoValor.Visible = false;
-            // 
-            // Estado
-            // 
-            this.Estado.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
-            this.Estado.HeaderText = "Ejemplo2";
-            this.Estado.Name = "Estado";
-            this.Estado.ReadOnly = true;
-            // 
-            // Ver
-            // 
-            this.Ver.HeaderText = "Ver";
-            this.Ver.Name = "Ver";
-            this.Ver.ReadOnly = true;
-            this.Ver.Resizable = System.Windows.Forms.DataGridViewTriState.True;
-            this.Ver.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic;
-            // 
-            // Edit
-            // 
-            this.Edit.HeaderText = "Editar";
-            this.Edit.Name = "Edit";
-            this.Edit.ReadOnly = true;
-            this.Edit.Resizable = System.Windows.Forms.DataGridViewTriState.True;
-            this.Edit.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic;
-            // 
-            // Eliminar
-            // 
-            this.Eliminar.HeaderText = "Eliminar";
-            this.Eliminar.Name = "Eliminar";
-            this.Eliminar.ReadOnly = true;
             // 
             // Btn_DetalleBovino
             // 
@@ -227,10 +71,10 @@
             this.Btn_DetalleBovino.IconColor = System.Drawing.Color.White;
             this.Btn_DetalleBovino.IconFont = FontAwesome.Sharp.IconFont.Auto;
             this.Btn_DetalleBovino.IconSize = 35;
-            this.Btn_DetalleBovino.Location = new System.Drawing.Point(825, 11);
-            this.Btn_DetalleBovino.Margin = new System.Windows.Forms.Padding(2);
+            this.Btn_DetalleBovino.Location = new System.Drawing.Point(1100, 14);
+            this.Btn_DetalleBovino.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Btn_DetalleBovino.Name = "Btn_DetalleBovino";
-            this.Btn_DetalleBovino.Size = new System.Drawing.Size(177, 35);
+            this.Btn_DetalleBovino.Size = new System.Drawing.Size(236, 43);
             this.Btn_DetalleBovino.TabIndex = 106;
             this.Btn_DetalleBovino.Text = "Detalle Bovino";
             this.Btn_DetalleBovino.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
@@ -238,17 +82,52 @@
             this.Btn_DetalleBovino.UseVisualStyleBackColor = false;
             this.Btn_DetalleBovino.Click += new System.EventHandler(this.Btn_DetalleBovino_Click);
             // 
+            // Dgv_ganado
+            // 
+            this.Dgv_ganado.AllowUserToAddRows = false;
+            this.Dgv_ganado.BackgroundColor = System.Drawing.Color.White;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.Padding = new System.Windows.Forms.Padding(2);
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.Dgv_ganado.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.Dgv_ganado.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.Dgv_ganado.Location = new System.Drawing.Point(15, 97);
+            this.Dgv_ganado.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Dgv_ganado.MultiSelect = false;
+            this.Dgv_ganado.Name = "Dgv_ganado";
+            this.Dgv_ganado.ReadOnly = true;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.Color.White;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.Color.Black;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.Dgv_ganado.RowHeadersDefaultCellStyle = dataGridViewCellStyle2;
+            this.Dgv_ganado.RowHeadersWidth = 16;
+            this.Dgv_ganado.RowTemplate.Height = 28;
+            this.Dgv_ganado.Size = new System.Drawing.Size(1321, 802);
+            this.Dgv_ganado.TabIndex = 107;
+            this.Dgv_ganado.TabStop = false;
+            // 
             // PGanado
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1013, 749);
+            this.ClientSize = new System.Drawing.Size(1351, 922);
+            this.Controls.Add(this.Dgv_ganado);
             this.Controls.Add(this.Btn_DetalleBovino);
-            this.Controls.Add(this.Dgv_usuarios);
             this.Controls.Add(this.btnagregarproducto);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "PGanado";
             this.Text = "PGanado";
-            ((System.ComponentModel.ISupportInitialize)(this.Dgv_usuarios)).EndInit();
+            this.Load += new System.EventHandler(this.PGanado_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.Dgv_ganado)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -256,20 +135,7 @@
         #endregion
 
         private FontAwesome.Sharp.IconButton btnagregarproducto;
-        public System.Windows.Forms.DataGridView Dgv_usuarios;
-        private System.Windows.Forms.DataGridViewButtonColumn btnseleccionar;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Id;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Usuario;
-        private System.Windows.Forms.DataGridViewTextBoxColumn NombreCompleto;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Correo;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Contraseña;
-        private System.Windows.Forms.DataGridViewTextBoxColumn IdRol;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Rol;
-        private System.Windows.Forms.DataGridViewTextBoxColumn EstadoValor;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Estado;
-        private System.Windows.Forms.DataGridViewButtonColumn Ver;
-        private System.Windows.Forms.DataGridViewButtonColumn Edit;
-        private System.Windows.Forms.DataGridViewButtonColumn Eliminar;
         private FontAwesome.Sharp.IconButton Btn_DetalleBovino;
+        public System.Windows.Forms.DataGridView Dgv_ganado;
     }
 }
